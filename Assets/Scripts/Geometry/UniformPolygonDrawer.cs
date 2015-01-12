6b11f017b4d633f577dcccac2e873f4ce5291d41
Harold Absalom <edward.absalom@gmail.com>
2015-01-12 19:46:22 +0100

    Fuller Tiles
    UniformPolygonDrawer now draws sides around the edges of the polygons
    (making them not polygons anymore, but I'll fix that naming oversight
    later).

--- Assets/Scripts/Geometry/UniformPolygonDrawer.cs
+++ Assets/Scripts/Geometry/UniformPolygonDrawer.cs
@@ -7,75 +7,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utils;
 
-public class UniformPolygonDrawer : MonoBehaviour {
-   #region fields
+namespace Geometry {
+   public class UniformPolygonDrawer : MonoBehaviour {
+      #region fields
 
-   [SerializeField]
-   private int _sides;
+      [SerializeField]
+      private int _sides;
 
-   [SerializeField]
-   private float _size;
+      [SerializeField]
+      private float _size;
 
-   [SerializeField]
-   private float _centreExtrusion;
+      [SerializeField]
+      private float _centreExtrusion;
 
-   #endregion fields
+      [SerializeField]
+      private float _height;
 
-   #region methods
+      #endregion fields
 
-   #region unity methods
+      #region methods
 
-   private void OnEnable() {
-      Mesh mesh = new Mesh();
+      #region unity methods
 
-      MeshFilter filter = GetComponent<MeshFilter>();
-      if (filter == null) {
-         filter = gameObject.AddComponent<MeshFilter>();
-      }
-      filter.mesh = mesh;
+      private void OnEnable() {
+         Mesh mesh = new Mesh();
 
-      CreateUniformPolygon(ref mesh);
-   }
+         MeshFilter filter = GetComponent<MeshFilter>();
+         if (filter == null) {
+            filter = gameObject.AddComponent<MeshFilter>();
+         }
+         filter.mesh = mesh;
 
-   #endregion unity methods
-
-   private void CreateUniformPolygon(ref Mesh mesh) {
-      List<Vector3> vertices = new List<Vector3>();
-      List<int> triangles = new List<int>();
-      List<Vector2> uvs = new List<Vector2>();
-      List<Vector3> normals = new List<Vector3>();
-
-      float step = Mathf.PI * 2 / _sides;
-
-      float u = Mathf.Sin((0) * step);
-      float v = Mathf.Cos((0) * step);
-
-      for (int i = 0; i < _sides; ++i) {
-         triangles.Add(vertices.Count);
-         vertices.Add(new Vector3(0, _centreExtrusion, 0));
-         uvs.Add(new Vector2(0.5f, 0.5f));
-
-         triangles.Add(vertices.Count);
-         vertices.Add(new Vector3(u * _size, 0, v * _size));
-         uvs.Add(new Vector2(u / 2.0f + 0.5f, v / 2.0f + 0.5f));
-
-         triangles.Add(vertices.Count);
-         u = Mathf.Sin((i + 1) * step);
-         v = Mathf.Cos((i + 1) * step);
-         vertices.Add(new Vector3(u * _size, 0, v * _size));
-         uvs.Add(new Vector2(u / 2.0f + 0.5f, v / 2.0f + 0.5f));
-
-         Vector3 n = Vector3.Normalize(Vector3.Cross(vertices[vertices.Count - 2] - vertices[vertices.Count - 3],
-                                                     vertices[vertices.Count - 1] - vertices[vertices.Count - 3]));
-         normals.Add(n); normals.Add(n); normals.Add(n);
+         CreateUniformPolygon(ref mesh);
       }
 
-      mesh.vertices = vertices.ToArray();
-      mesh.uv = uvs.ToArray();
-      mesh.normals = normals.ToArray();
-      mesh.triangles = triangles.ToArray();
-   }
+      #endregion unity methods
 
-   #endregion methods
+      private void CreateUniformPolygon(ref Mesh mesh) {
+         List<Vector3> vertices = new List<Vector3>();
+         List<int> triangles = new List<int>();
+         List<Vector2> uvs = new List<Vector2>();
+         List<Vector3> normals = new List<Vector3>();
+
+         float step = Mathf.PI * 2 / _sides;
+
+         float u = Mathf.Sin(0);
+         float v = Mathf.Cos(0);
+
+         for (int i = 0; i < _sides; ++i) {
+            // middle of polygon
+            vertices.Add(new Vector3(0, _height + _centreExtrusion, 0));
+            uvs.Add(new Vector2(0.5f, 0.5f));
+
+            // first point on border
+            AddBorderPoints(vertices, uvs, u, v);
+
+            // first half of side
+            AddSidePoints(vertices, uvs, u, v);
+
+            // second point on border
+            u = Mathf.Sin((i + 1) * step);
+            v = Mathf.Cos((i + 1) * step);
+            AddBorderPoints(vertices, uvs, u, v);
+
+            // second half of side
+            AddSidePoints(vertices, uvs, u, v);
+
+            triangles.AddInOrderWithOffset(vertices.Count, -7, -6, -3, -5, -4, -2, -1, -2, -4);
+
+            Vector3 topNormal = Vector3.Normalize(Vector3.Cross(vertices[vertices.Count - 6] - vertices[vertices.Count - 7],
+                                                                vertices[vertices.Count - 3] - vertices[vertices.Count - 7]));
+            Vector3 sideNormal = Vector3.Normalize(Vector3.Cross(vertices[vertices.Count - 1] - vertices[vertices.Count - 4],
+                                                                 vertices[vertices.Count - 2] - vertices[vertices.Count - 4]));
+            normals.AddInOrder(topNormal, topNormal, sideNormal, sideNormal, topNormal, sideNormal, sideNormal);
+         }
+
+         mesh.vertices = vertices.ToArray();
+         mesh.uv = uvs.ToArray();
+         mesh.normals = normals.ToArray();
+         mesh.triangles = triangles.ToArray();
+      }
+
+      private void AddBorderPoints(List<Vector3> vertices, List<Vector2> uvs, float u, float v) {
+         vertices.Add(new Vector3(u * _size, _height, v * _size));
+         uvs.Add(new Vector2(u / 2.0f + 0.5f, v / 2.0f + 0.5f));
+      }
+
+      private void AddSidePoints(List<Vector3> vertices, List<Vector2> uvs, float u, float v) {
+         vertices.Add(new Vector3(u * _size, _height, v * _size));
+         uvs.Add(new Vector2(0, 0));
+
+         vertices.Add(new Vector3(u * _size, 0, v * _size));
+         uvs.Add(new Vector2(0, 0));
+      }
+
+      #endregion methods
+   }
 }
---
+++ Assets/Scripts/Utils/ExtentionMethods.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------
+// Filename: ExtentionMethods.cs
+// Author:   Harold Absalom
+// Licence:  GNU General Public License
+// -----------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Utils {
+   public static class ExtentionMethods {
+      public static void AddInOrder<T>(this List<T> list, params T[] items) {
+         foreach (T item in items) {
+            list.Add(item);
+         }
+      }
+
+      public static void AddInOrderWithOffset(this List<int> list, int offset, params int[] items) {
+         foreach (int item in items) {
+            list.Add(offset + item);
+         }
+      }
+   }
+}
