c7f512e2b82bfe2dbc0c8b54a31ceb3c0d1baccc
Harold Absalom <edward.absalom@gmail.com>
2017-06-23 17:44:36 +1000

    Cleaned up Backboard code.

--- Assets/Scripts/Utils/Backboard.cs
+++ Assets/Scripts/Utils/Backboard.cs
@@ -16,9 +16,7 @@ namespace Utils {
       [SerializeField]
       private float _thickness = 0.5f;
 
-      private GameObject _backboard;
       private BoxCollider _collider;
-      private ClickPasser _clickPasser;
       private Camera _camera;
 
       #endregion fields
@@ -28,28 +26,28 @@ namespace Utils {
       #region unity methods
 
       private void Start() {
-         _backboard = new GameObject("Backboard");
-         _backboard.transform.parent = transform;
-
-         _collider = _backboard.AddComponent<BoxCollider>();
-
-         _clickPasser = _backboard.AddComponent<ClickPasser>();
-         _clickPasser.Clicked += OnBackboardClicked;
-
          _camera = GetComponent<Camera>();
+
+         _collider = gameObject.AddComponent<BoxCollider>();
       }
 
       private void Update() {
-         _backboard.transform.localPosition = new Vector3(0.0f, 0.0f, _camera.farClipPlane + (_thickness / 2.0f));
+         _collider.center = new Vector3(0.0f, 0.0f, _camera.farClipPlane + (_thickness / 2.0f));
 
-         Vector3 bottomLeft = _camera.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, _camera.farClipPlane));
-         Vector3 topRight = _camera.ScreenToWorldPoint(new Vector3(_camera.pixelWidth, _camera.pixelHeight, _camera.farClipPlane));
-         _collider.size = new Vector3(topRight.x - bottomLeft.x, _thickness, bottomLeft.z - topRight.z);
+         Vector3 worldBottomLeft = _camera.ViewportToWorldPoint(new Vector3(0.0f, 0.0f, _camera.farClipPlane));
+         Vector3 worldTopRight   = _camera.ViewportToWorldPoint(new Vector3(1.0f, 1.0f, _camera.farClipPlane));
+
+         Vector3 bottomLeft = transform.worldToLocalMatrix.MultiplyPoint(worldBottomLeft);
+         Vector3 topRight   = transform.worldToLocalMatrix.MultiplyPoint(worldTopRight);
+
+         _collider.size = new Vector3(Mathf.Abs(topRight.x - bottomLeft.x),
+                                      Mathf.Abs(topRight.y - bottomLeft.y),
+                                      _thickness);
       }
 
       #endregion unity methods
 
-      private void OnBackboardClicked(object sender, EventArgs e) {
+      private void OnMouseDown() {
          Debug.Log("Backboard");
       }
 
